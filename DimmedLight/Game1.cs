e294901f8aebdd3dcebee2e176b792c2ed1ccec4
Idea-Coding <idea.a.123456@gmail.com>
2025-10-06 01:22:45 +0700

    Update Game1.cs

--- DimmedLight/Game1.cs
+++ DimmedLight/Game1.cs
@@ -5,22 +5,17 @@ using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
-using System.Drawing;
 using System.Reflection.Metadata;
-using DimmedLight.Gameplay.MainMenu;
 
-namespace DimmedLight
+namespace DimmedLight //git
 {
-    public class Game1 : Game //local
+    public class Game1 : Game
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        public GraphicsDeviceManager _graphics;
+        public SpriteBatch _spriteBatch;
         private SpriteFont font;
 
-        private Screen _currentScreen;
-
-        // --- FIX #2: สร้าง Public Property ให้คลาสอื่นเรียกใช้ _graphics ได้ ---
-        public GraphicsDeviceManager Graphics => _graphics;
+        private Gameplay _gamePlay;
 
         private Screen _currentScreen;
         private Screen _previousScreen;
@@ -32,54 +27,33 @@ namespace DimmedLight
             _graphics = new GraphicsDeviceManager(this);
             _graphics.PreferredBackBufferWidth = 1920;
             _graphics.PreferredBackBufferHeight = 1080;
+            _graphics.IsFullScreen = false;
             _graphics.ApplyChanges();
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
 
-        public void ChangeScreen(Screen newScreen)
-        {
-            _currentScreen = newScreen;
-            if (Content != null && GraphicsDevice != null)
-            {
-                _currentScreen.LoadContent();
-            }
-        }
-
-        // --- FIX #3: สร้างเมธอด SetFullScreen ---
-        public void SetFullScreen(bool isFullScreen)
-        {
-            _graphics.IsFullScreen = isFullScreen;
-            _graphics.ApplyChanges();
-        }
-
         protected override void Initialize()
         {
-            _graphics.PreferredBackBufferWidth = 1920;
-            _graphics.PreferredBackBufferHeight = 1080;
-            _graphics.ApplyChanges();
-
+            _gamePlay = new Gameplay(this, _graphics);
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            _gamePlay.LoadContent();
+            var menuScreen = new MenuScreen(this, _graphics, GraphicsDevice, Content);
+            ChangeScreen(menuScreen);
         }
-
         protected override void Update(GameTime gameTime)
         {
-            _gamePlay.Update(gameTime);
+            _currentScreen?.Update(gameTime);
             base.Update(gameTime);
         }
-
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-            _gamePlay.Draw(gameTime);
-            //
+            _currentScreen?.Draw(gameTime, _spriteBatch);
             base.Draw(gameTime);
         }
         public void ChangeScreen(Screen newScreen)
