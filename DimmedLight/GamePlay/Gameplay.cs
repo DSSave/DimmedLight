2d5d513e0ebd87ef27e9514edc79edc0a47e4838
Idea-Coding <idea.a.123456@gmail.com>
2025-10-06 04:57:30 +0700

    Add stepalange to gameplay
    gamefont02 size30

--- DimmedLight/GamePlay/Gameplay.cs
+++ DimmedLight/GamePlay/Gameplay.cs
@@ -96,7 +96,7 @@ namespace DimmedLight.GamePlay
             attackProjecTex = game.Content.Load<Texture2D>("bullet2");
             parryProjecTex = game.Content.Load<Texture2D>("bullet1");
 
-            font = game.Content.Load<SpriteFont>("gameFont");
+            font = game.Content.Load<SpriteFont>("gameFont02");
             hellCloakTheme = game.Content.Load<Texture2D>("ThemeEvent");
             tutorialImage = game.Content.Load<Texture2D>("tutorialEvent");
             pauseImage = game.Content.Load<Texture2D>("Frame");
